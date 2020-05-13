fee1f8042e1d4779cf8a6b4172eeba7db12502da
Liliya Gizatullina <glilia99@mail.ru>
2020-05-13 15:31:52 +0300

    commit от Лили:)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import controller.MoveController;
+import model.Field;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Liliya");
+        Field f = new Field(10);
+        System.out.println(MoveController.whoseTurn(f));
+        MoveController.makeMove(f, 4, 4);
+        System.out.println(MoveController.whoseTurn(f));
+        MoveController.makeMove(f, 4, 4);
+        System.out.println(MoveController.whoseTurn(f));
+
     }
+
 }
---
+++ src/main/java/model/Game.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Game {
+    private  Player player1;
+    private  Player player2;
+    private  Field field;
+
+    public Game(Player player1, Player player2, Field field){
+        this.player1=player1;
+        this.player2=player2;
+        this.field=field;
+
+    }
+}
